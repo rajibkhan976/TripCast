7381c2a52320827a50032b29ca2269a7f536fa37
unknown <rajibkhan976@gmail.com>
2019-04-25 15:55:52 +0200

    Tripplanner component created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LogInComponent from './Components/LogInComponent';
+import TripPlannerComponent from './Components/TripPlannerComponent';
 
 class App extends Component {
   render() {
     return (
-      <LogInComponent/>
+      <div>
+        <LogInComponent/>
+        <TripPlannerComponent/>
+      </div>
     );
   }
 }
