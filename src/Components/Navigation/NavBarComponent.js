8e279ef21f8f9d3ae7c45c77e6bc4f2986f464dc
valentinapalma <valentinapalma@hotmail.se>
2019-04-29 14:01:50 +0200

    fixed css

--- src/Components/Navigation/NavBarComponent.js
+++ src/Components/Navigation/NavBarComponent.js
@@ -9,17 +9,17 @@ class NavBarComponent extends Component {
         return (
             <div>
                 <div id="google_translate_element" className={style.google}/>
-                <Navbar className={style.what} expand="lg">
+                <Navbar className={style.navigationContainer} expand="lg">
                     <Navbar.Brand>
                         <h1 className={style.logo}>TripCast</h1>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <NavLink className={style.list} activeClassName={style.activeLink} to="/home">Home</NavLink>
-                        <NavLink className={style.list} activeClassName={style.activeLink} to="/mypage">Profile</NavLink><br/>
-                        <NavLink className={style.list} activeClassName={style.activeLink} to="/planner">Trip Planner</NavLink><br/>
-                        <NavLink className={style.list} activeClassName={style.activeLink} to="/recent">Recent</NavLink>
-                        <NavLink className={style.list} activeClassName={style.activeLink} to="/about">About</NavLink>
+                        <NavLink className={style.navLinks} activeClassName={style.activeLink} to="/home">Home</NavLink>
+                        <NavLink className={style.navLinks} activeClassName={style.activeLink} to="/mypage">Profile</NavLink><br/>
+                        <NavLink className={style.navLinks} activeClassName={style.activeLink} to="/planner">Trip Planner</NavLink><br/>
+                        <NavLink className={style.navLinks} activeClassName={style.activeLink} to="/recent">Recent</NavLink>
+                        <NavLink className={style.navLinks} activeClassName={style.activeLink} to="/about">About</NavLink>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
